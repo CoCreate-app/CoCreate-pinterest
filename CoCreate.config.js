187860629dae4c660c2ba29788677091593ec0c4
frankpagan <frank@cocreate.app>
2021-02-14 21:01:54 +0000

    clean tree

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -9,7 +9,7 @@ module.exports = {
     sources: [{
             path: "./docs/index.html",
             collection: "files",
-            document_id: "",
+            document_id: "60298ef8e979fb17407e25bf",
             key: "html",
             data:{
                 name: "Pinterest Doc",
@@ -19,10 +19,10 @@ module.exports = {
    
     crud: [{
             collection: "routes",
-            document_id: "",
+            document_id: "60298ef8e979fb17407e25be",
             data:{
                 collection: "files",
-                document_id: "",
+                document_id: "60298ef8e979fb17407e25bf",
                 name: "html",
                 domains: ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
                 route: "/docs/pinterest",
