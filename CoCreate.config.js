dc1bff9f55775277d883a8557d170cc8d5cff7a7
Frank Pagan <frank@cocreate.app>
2022-11-21 19:22:03 +0000

    fix: replaced document_id with document._id

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -10,7 +10,7 @@ module.exports = {
             "collection": "files",
             "document_id": "60298ef8e979fb17407e25bf",
             "key": "src",
-            "data":{
+            "document": {
                 "name": "index.html",
                 "path": "/docs/pinterest/index.html",
                 "domains": [
