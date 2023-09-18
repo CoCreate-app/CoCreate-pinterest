a1ea7df8987a2b5cd7cd3fb8b31798bac9915d9d
frankpagan <frank@cocreate.app>
2023-09-17 19:11:38 -0600

    fix:  Add path and pathname

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -8,17 +8,16 @@ module.exports = {
             "object": {
                 "_id": "60298ef8e979fb17407e25bf",
                 "name": "index.html",
-                "path": "/docs/pinterest/index.html",
+                "path": "/docs/pinterest",
+                "pathname": "/docs/pinterest/index.html",
                 "src": "{{./docs/index.html}}",
                 "host": [
                     "*",
                     "general.cocreate.app"
                 ],
-                "directory": "/docs/pinterest",
-                "parentDirectory": "{{parentDirectory}}",
+                "directory": "pinterest",
                 "content-type": "{{content-type}}",
-                "public": "true",
-                "website_id": "644d4bff8036fb9d1d1fd69c"
+                "public": "true"
             }
         }
     ]
