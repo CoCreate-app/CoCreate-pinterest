2c8ff478eca19f111e55ccfc5225ee7415754ae1
frankpagan <frank@cocreate.app>
2023-11-25 09:38:17 -0600

    fix: getOrg() renamed to getOrganization()

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ class CoCreatePinterest {
         let pinterest = false;
 
         try {
-            let org = await api.getOrg(data, this.name);
+            let org = await api.getOrganization(data, this.name);
             if (params.environment) {
                 environment = params['environment'];
                 delete params['environment'];
